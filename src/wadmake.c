238a35fac66e7a386f4a3d137d57c913081f313a
Alex Mayfield <alexmax2742@gmail.com>
2015-01-13 23:58:11 -0500

    Dump .lua files to C headers while building.
    We want to be able to write "business logic" in Lua, but we also want
    wadmake to be a single-file utility.  So during the build process, we
    use xxd to dump Lua source files to C headers that we can then include
    and load directly into the Lua environment.

--- src/wadmake.c
+++ src/wadmake.c
@@ -1,29 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <bstrlib.h>
 #include <lua.h>
 #include <lauxlib.h>
 #include <lualib.h>
 
+#include "init.lua.h"
+
 typedef enum {
 	OK,
 	ERROR
 } error;
 
+lua_State* Lua;
+
+static void Cleanup() {
+	lua_close(Lua);
+}
+
+void FatalError(const bstring error) {
+	fprintf(stderr, "wadmake: %s\n", error->data);
+	Cleanup();
+	exit(EXIT_FAILURE);
+}
+
+void CFatalError(const char* error) {
+	fprintf(stderr, "wadmake: %s\n", error);
+	Cleanup();
+	exit(EXIT_FAILURE);
+}
+
 int main(int argc, char* argv) {
 	const char* filename = "wadmakefile";
 
-	lua_State* L = luaL_newstate();
+	Lua = luaL_newstate();
+	luaL_requiref(Lua, "_G", luaopen_base, 1);
+	luaL_requiref(Lua, LUA_LOADLIBNAME, luaopen_package, 1);
+	lua_pop(Lua, 1);
 
-	if (luaL_dofile(L, filename) == 1) {
-		printf("wadmake: error while parsing %s\n");
-		goto cleanup;
+	if (luaL_loadbuffer(Lua, src_lua_init_lua, src_lua_init_lua_len, "init") != LUA_OK) {
+		FatalError(bformat("internal lua error\n%s", lua_tostring(Lua, -1)));
 	}
 
-	lua_close(L);
-	return 0;
+	if (lua_pcall(Lua, 0, LUA_MULTRET, 0) != LUA_OK) {
+		FatalError(bformat("internal lua runtime error\n%s", lua_tostring(Lua, -1)));
+	}
 
-cleanup:
-	lua_close(L);
-	return 1;
+	if (luaL_dofile(Lua, filename) == 1) {
+		FatalError(bformat("error while parsing %s", filename));
+	}
+
+	return EXIT_SUCCESS;
 }
