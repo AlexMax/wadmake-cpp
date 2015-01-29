174331d7bce8cd303158ab9e7f8ed1c620df8930
Alex Mayfield <alexmax2742@gmail.com>
2015-01-28 21:29:15 -0500

    Fix Windows weirdness.

--- src/lua/lwad.lua
+++ src/lua/lwad.lua
@@ -2,7 +2,7 @@
 local mod = {}
 
 function mod.openwad(filename)
-	local file = io.open(filename)
+	local file = io.open(filename, 'rb')
 	local data = file:read('a')
 	return wad.readwad(data)
 end
